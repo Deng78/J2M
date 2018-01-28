b82d48c0bfd9b17037b3fe4d6d01d2f7d0985425
Andrew Sharpe <asharpe@squiz.net>
2018-01-28 13:23:56 +1000

    fix strikethrough as per https://github.com/chjj/marked/issues/1038

--- src/J2M.js
+++ src/J2M.js
@@ -25,7 +25,7 @@
 		input = input.replace(/\^([^^]*)\^/g, '<sup>$1</sup>');
 		input = input.replace(/~([^~]*)~/g, '<sub>$1</sub>');
 		input = input.replace(/-{3,}/g, "---");
-		input = input.replace(/([^-])-([^-]+)-/g, '$1<s>$2</s>');
+		input = input.replace(/([^-])-([^-]+)-/g, '$1~~$2~~');
 
 		input = input.replace(/\{code(:([a-z]+))?\}([^]*?)\{code\}/gm, '```$2$3```');
 		input = input.replace(/\{quote\}([^]*)\{quote\}/gm, function(match, content) {
